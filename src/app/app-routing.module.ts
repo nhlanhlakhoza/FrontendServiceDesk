22491ace05a97af4f35ca1b67410f29a7469b7e6
Kwazikonke <21707921@dut4life.ac.za>
2023-04-06 21:51:22 +0200

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketsComponent } from './components/tickets/tickets.component';
 import { LoginComponent } from './pages/login/login.component';
-import { SubscriptionsComponent } from './components/subscriptions/subscriptions.component';
 import { UserHomepageComponent } from './pages/user-homepage/user-homepage.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 const routes: Routes = [
   {path:"",component:UserHomepageComponent},
   {path:"tickets",component:TicketsComponent},
   {path:"login",component:LoginComponent},
-  {path:"subscriptions",component:SubscriptionsComponent},
+  {path:"subscriptions",component:NavbarComponent},
   
 ];
 
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubscriptionsComponent } from 'src/app/components/subscriptions/subscriptions.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  defaultComponent:any;
+  constructor() { 
+    this.defaultComponent=SubscriptionsComponent;
+  }
+  ngOnInit(): void {
+  }
 }
